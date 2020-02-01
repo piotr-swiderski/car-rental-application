2f0e8afca83917942e2621cb0053a2a816f891d1
Piotr Świderski <team1996@interia.pl>
2020-02-01 23:37:55 +0100

    Add Model dao and implementation

---
+++ src/main/java/dao/ModelDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import model.Model;
+
+import java.util.Set;
+
+public interface ModelDao {
+
+    void saveModel(Model model);
+
+    Set<Model> getModelByType(String type);
+
+    Set<Model> getModelByName(String name);
+
+    Set<Model> getAllModels();
+
+
+}
---
+++ src/main/java/dao/impl/ModelDaoImpl.java
@@ -0,0 +1,37 @@
+package dao.impl;
+
+import dao.ModelDao;
+import hibernate.util.HibernateUtil;
+import model.Model;
+
+import javax.persistence.TypedQuery;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ModelDaoImpl extends HibernateUtil implements ModelDao {
+    @Override
+    public void saveModel(Model model) {
+        save(model);
+    }
+
+    @Override
+    public Set<Model> getModelByType(String type) {
+        TypedQuery<Model> query = getEntityManager().createQuery("select m from Model m where m.type = :type", Model.class);
+        query.setParameter("type", type);
+        return new HashSet<>(query.getResultList());
+    }
+
+    @Override
+    public Set<Model> getModelByName(String name) {
+        TypedQuery<Model> query = getEntityManager().createQuery("select m from Model m where m.name = :name", Model.class);
+        query.setParameter("name", name);
+        return new HashSet<>(query.getResultList());
+    }
+
+
+    @Override
+    public Set<Model> getAllModels() {
+        TypedQuery<Model> query = getEntityManager().createQuery("select m from Model m", Model.class);
+        return new HashSet<>(query.getResultList());
+    }
+}
